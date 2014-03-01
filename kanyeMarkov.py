67c614377625f439b3ac49b3ce7d74528c866554
Timothy Lenardo <telenardo@rrdhcp-10-32-143-141.redrover.cornell.edu>
2014-03-01 16:29:52 -0500

    Restructuring for ease of understanding

--- kanyeMarkov.py
+++ kanyeMarkov.py
@@ -8,44 +8,56 @@ import csv
 from collections import deque
 from xml.etree import ElementTree
 
+###############################################################################
 
-#r = requests.get('http://developer.echonest.com/api/v4/song/search?api_key=EYIQEWOOIAQIWILVH&artist=kanye+west&results=100')
-#json_response = r.json()
-#songs = json_response['response']['songs']
-#for song in songs:
-#  title = song['title']
-#  stripped = re.sub(r'\([^)]*\)', '', title)
-#  stripped = re.sub(r'\)', '', stripped)
-#  stripped = re.sub(r'\'', '', stripped)
-#  split = stripped.split()
-#  first = 1
-#  conca = ""
-#  for word in split:
-#    if first == 1:
-#      conca = conca + word
-#      first = 0
-#    else:
-#      conca = conca + "+" + word
-#  url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=kanye%20west&song=" + conca
-#  print url
-#  r = requests.get(url)
-#  tree = ElementTree.fromstring(r.content)
-#  print tree.findall('SearchLyricResult')
-#  for result in tree.findall('SearchLyricResult'):
-#    print result
+# CONSTANTS
+# Edit these values to change the results!!
 
-vowels = ['AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'EH', 'ER', 'EY', 'IH', 'IY', 'OW', 'OY', 'UH', 'UW']
+# The file of lyrics used to train the markov chain.
+LYRIC_FILE = 'KanyeLyricFiles/kywt.txt'
+
+# The pronunciation dictionary used to create rhymes. This must contain all of
+# the words in the lyrics file
+RHYMING_DICTIONARY = 'KanyeRhymingDictionary/ky2.dict'
+
+# Number of lyrics produced
+NUMBER_LINES = 4
+
+# The number of words per lyric
+MAX_WORDS = 8
+MIN_WORDS = 5
+
+###############################################################################
+
+# Vowels used to determine whether words rhyme!
+vowels = [
+  'AA',
+  'AE',
+  'AH',
+  'AO',
+  'AW',
+  'AY',
+  'EH',
+  'ER',
+  'EY',
+  'IH',
+  'IY',
+  'OW',
+  'OY',
+  'UH',
+  'UW'
+]
 
 # Create an instance of the markov chain. By default, it uses MarkovChain.py's location to
 # store and load its database files to. You probably want to give it another location, like so:
 mc = MarkovChain("./markov")
 # To generate the markov chain's language model, in case it's not present
-with open ("kywt.txt", "r") as myfile:
+with open (LYRIC_FILE, "r") as myfile:
   data=myfile.read().replace('\n', '\n')
 mc.generateDatabase(data, '\n')
 
 rhyming_dictionary = {}
-with open('ky2.dict', 'rb') as csvfile:
+with open(RHYMING_DICTIONARY, 'rb') as csvfile:
   spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
   for row in spamreader:
     rhyming_dictionary[row[0]] = row[1:]
@@ -61,7 +73,7 @@ def lastVowel(syllables):
 
 d = deque()
 # generate 4 lines
-for i in range(0,4):
+for i in range(0,NUMBER_LINES):
   flag = 0
   length = 0
   stri = ""
@@ -85,18 +97,20 @@ for i in range(0,4):
     while length == 0:
       stri = mc.generateString()
       stri_split = stri.split(' ')
-      if (len(stri_split) < 8 and len(stri_split) > 5):
+      if (len(stri_split) < MAX_WORDS and len(stri_split) > MIN_WORDS):
         length = 1
+    # Determine the last word, syllables, and vowel
     last_word = stri_split[len(stri_split) - 1].upper()
     last_word_syl = rhyming_dictionary[last_word]
     last_vowel = lastVowel(last_word_syl)
-    #last_syl_2 = last_word_syl[len(last_word_syl) - 1]
-    #if last_syl_2 == last_syl:
+
+    # Check the current line for rhyming against all others generated
     for (strin, word, vowel) in gens:
       if last_vowel == vowel and last_vowel != None and word != last_word:
         print strin
         print stri
         flag = 1
+    # Otherwise add to the list of generated lines
     gens.append((stri, last_word, last_vowel))
 
 
